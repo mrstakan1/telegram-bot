7f57c491e2ffa03ba8e7eb6525b6930f39f08b80
Pavel <88883654+mrstakan1@users.noreply.github.com>
2022-08-24 12:50:00 +0300

    Added selenium configs

--- config.py
+++ config.py
@@ -1,1 +1,26 @@
+from selenium import webdriver                         # Импортируем Веб Драйвер
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys     # Импортируем обработчик нажатия кнопок
+#Bot
 TOKEN = '5658028624:AAEHCBcaDE8ozuU1rXe0b9eyVnMYqHturxE'
+
+#SELENIUM
+# Настройки драйвера
+options = webdriver.ChromeOptions()
+
+# Добавляем User-Agent в настройки
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36")
+# Фоновый режим
+options.add_argument("--headless") 
+# Отключаем режим Веб Драйвера
+options.add_argument("--disable-blink-features=AutomationControlled")
+
+# Подавление Getting Default Adapter failed
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+# Создание Веб Драйвера
+driver = webdriver.Chrome()
+#     executable_path="C:\\VS_PROJ\\TeleBot\\chromedriver.exe",   # Указываем путь до драйвера
+#     options=options   # Устанавливаем настройки
+#     )
+
